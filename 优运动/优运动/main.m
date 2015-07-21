281f512414a2e960feadd0db15bf063ed301c239
sam <samyang1107@icloud.com>
2015-07-21 10:15:45 +0800

    添加项目

---
+++ 优运动/优运动/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  优运动
+//
+//  Created by 杨森 on 15/7/21.
+//  Copyright (c) 2015年 samyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
